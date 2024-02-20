6cb09046bc0ad8b8435c465523e7c6dcce9ab5cf
FelipeGuimaraes1 <fil-2014@hotmail.com>
2024-02-20 20:14:08 -0300

    Terceiro Commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localost",
+  username: "postgres",
+  password: "postgres",
+  database: "devburger",
+  define: {
+    timespamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
